dca12d7302abdf56f077166d5fe47372ec9b2031
ksvirkou <ksvirkou@hubspot.com>
2021-03-17 14:21:52 +0300

    add properties

--- php/src/Commands/Objects/GetCommand.php
+++ php/src/Commands/Objects/GetCommand.php
@@ -45,18 +45,26 @@ class GetCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $hubspot = HubspotClientHelper::createFactory();
         $objectTypeId = $input->getArgument('objectTypeId');
+        $schema = $hubspot->crm()->schemas()->CoreApi()->getById($objectTypeId);
 
         if (!empty($input->getOption('id'))) {
             $id = $input->getOption('id');
+
             $io->writeln("Getting CRM object instance from schema by id: {$id}");
 
-            $response = $hubspot->crm()->objects()->basicApi()->getById($objectTypeId, $id);
+            $response = $hubspot->crm()->objects()->basicApi()->getById(
+                $objectTypeId,
+                $id,
+                $this->propertiesNamesToString($schema->getProperties())
+            );
 
             $io->info($response);
         } else {
             $io->writeln('Getting all object instances from schema...');
 
-            $response = $hubspot->crm()->objects()->basicApi()->getPage($objectTypeId);
+            $response = $hubspot->crm()->objects()->basicApi()
+                ->getPage($objectTypeId, 10, null, implode(',', $schema->getRequiredProperties()))
+            ;
 
             if (count($response->getResults()) > 0) {
                 $io->listing($response->getResults());
@@ -66,5 +74,17 @@ class GetCommand extends Command
         }
 
         return Command::SUCCESS;
+    }
+
+    protected function propertiesNamesToString(array $properties)
+    {
+        $names = array_map(
+            function ($property) {
+                return $property->getName();
+            },
+            $properties
+        );
+
+        return implode(',', $names);
     }
 }
