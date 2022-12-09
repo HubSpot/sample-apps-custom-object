61a62f1734d8d1cfddd0b57b4d6995ac85fc14bc
alexey <azheltkovskiy@hubspot.com>
2022-12-09 16:26:44 +0300

    update pythan sample

--- python/cli.py
+++ python/cli.py
@@ -7,9 +7,11 @@ from hubspot.crm.objects.models import SimplePublicObjectInput
 from hubspot.crm.schemas.models import ObjectSchemaEgg
 from hubspot.crm.schemas.models import ObjectTypeDefinitionPatch
 
+
 def access_token():
-  load_dotenv()
-  return os.environ['ACCESS_TOKEN']
+    load_dotenv()
+    return os.environ['ACCESS_TOKEN']
+
 
 parser = argparse.ArgumentParser(description='Parse Hubspot API arguments')
 parser.add_argument('-a', '--api', help='Can be "schema" or "object"')
@@ -20,25 +22,27 @@ parser.add_argument('-p', '--properties', help='Properties of object')
 parser.add_argument('-k', '--kwargs', help='kwargs to pass')
 args = parser.parse_args()
 
-if (args.api is None):
-  raise Exception('Please, provide api with -a or --api. See --help to get more info.')
+if args.api is None:
+    raise Exception('Please, provide api with -a or --api. See --help to get more info.')
 
-if (args.method is None):
-  raise Exception('Please, provide method with -m or --method. See --help to get more info.')
+if args.method is None:
+    raise Exception('Please, provide method with -m or --method. See --help to get more info.')
 
 api_client = HubSpot(access_token=access_token())
 api = api_client.crm.objects.basic_api if args.api == 'object' else api_client.crm.schemas.core_api
 
 kwargs = vars(args)
-filtered_kwargs = dict((k, v) for k, v in kwargs.items() if v is not None and k != 'api' and k != 'method' and k != 'properties')
-if (args.properties is not None):
-  properties = json.loads(args.properties)
-  if (args.api == 'object'):
-    filtered_kwargs['simple_public_object_input'] = SimplePublicObjectInput(properties=properties)
-  elif (args.method == 'create'):
-    filtered_kwargs['object_schema_egg'] = ObjectSchemaEgg(**properties)
-  else:
-    filtered_kwargs['object_type_definition_patch'] = ObjectTypeDefinitionPatch(**properties)
+filtered_kwargs = {
+  k: v for k, v in kwargs.items() if v is not None and k != 'api' and k != 'method' and k != 'properties'
+}
+if args.properties is not None:
+    properties = json.loads(args.properties)
+    if args.api == 'object':
+        filtered_kwargs['simple_public_object_input'] = SimplePublicObjectInput(properties=properties)
+    elif args.method == 'create':
+        filtered_kwargs['object_schema_egg'] = ObjectSchemaEgg(**properties)
+    else:
+        filtered_kwargs['object_type_definition_patch'] = ObjectTypeDefinitionPatch(**properties)
 
 result = getattr(api, args.method)(**filtered_kwargs)
 print(result)
